814dc22995a81c86c1b232188f308c6709e6bd5f
negu63 <negu6633@gmail.com>
2022-12-06 01:24:41 +0900

    feat: token api subscribe logic 변경

--- server/src/api/routes/token.ts
+++ server/src/api/routes/token.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response } from "express";
 import admin from "firebase-admin";
 import Token from "../../models/token";
+import { STREAMER_LIST } from "../../constants/stream";
 
 const route = Router();
 
@@ -16,15 +17,32 @@ export default (app: Router) => {
     return res.json({ result: "Token registration successful." }).status(200);
   });
 
-  route.post("/subscribe", (req: Request, res: Response) => {
-    admin
-      .messaging()
-      .subscribeToTopic(req.body.token, req.body.topic)
-      .then((res) => console.log("success to sub", res))
-      .catch((err) => console.log("err : ", err));
+  route.post("/subscribe", async (req: Request, res: Response) => {
+    const token: string | string[] = req.body.token;
+    const topics: string[] = req.body.topics;
+    await unsubscribeAllTopic(token);
+    await Promise.all(
+      topics.map((topic: string) => subscribeTopic(token, topic))
+    );
 
     return res
       .json({ result: "You have been successfully subscribed." })
       .status(200);
   });
+};
+
+const unsubscribeAllTopic = async (token: string | string[]) => {
+  await Promise.all(
+    STREAMER_LIST.map((topic) =>
+      admin.messaging().unsubscribeFromTopic(token, topic)
+    )
+  );
+};
+
+const subscribeTopic = (token: string | string[], topic: string) => {
+  admin
+    .messaging()
+    .subscribeToTopic(token, topic)
+    .then((res) => console.log("success to sub", res))
+    .catch((err) => console.log("err : ", err));
 };
