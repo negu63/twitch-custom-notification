46bcb93c8c104d7106ba8b034d48bed9587bb16b
negu63 <negu6633@gmail.com>
2022-12-05 15:39:35 +0900

    chore: config 추가

---
+++ server/config/index.ts
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+const envFound = dotenv.config();
+
+if (envFound.error) {
+  // This error should crash whole process
+
+  throw new Error("⚠️  Couldn't find .env file  ⚠️");
+}
+
+export default {
+  port: parseInt(process.env.PORT || "3000", 10),
+  databaseURL: process.env.MONGODB_URI,
+};
