8cef411753fec671b1a89f97ee7e4e068de6898f
negu63 <negu6633@gmail.com>
2022-12-05 23:55:51 +0900

    feat: scheduler loader 적용

--- server/src/loaders/index.ts
+++ server/src/loaders/index.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import mongooseLoader from "./mongoose";
 import expressLoader from "./express";
+import schedulerLoader from "./scheduler";
 
 export default async ({ expressApp }: { expressApp: express.Application }) => {
   await mongooseLoader();
   console.info("✌️ DB loaded and connected!");
 
   await expressLoader({ app: expressApp });
-  console.info('✌️ Express loaded');
+  console.info("✌️ Express loaded");
+
+  schedulerLoader();
+  console.info("✌️ Scheduler loaded");
 };
