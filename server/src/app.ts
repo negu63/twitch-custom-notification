cbe842b2a929f9fbec92e729d3ab067197b842e9
negu63 <negu6633@gmail.com>
2022-12-05 16:32:25 +0900

    feat: app 추가

---
+++ server/src/app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+
+import config from "./config";
+
+async function startServer() {
+  const app = express();
+
+  await require("./loaders").default({ expressApp: app });
+
+  app
+    .listen(config.port, () => {
+      console.info(`
+      ################################################
+      🛡️  Server listening on port: ${config.port} 🛡️
+      ################################################
+    `);
+    })
+    .on("error", (err) => {
+      process.exit(1);
+    });
+}
+
+startServer();
